8bba8bb157923a627cd36302d3dbfc6821bd1a80
Augustin Luna <lunaa@cbio.mskcc.org>
2016-03-02 01:33:00 +0000

    version 0.2.2

--- R/setCacheRootPath.R
+++ R/setCacheRootPath.R
@@ -1,12 +1,16 @@
 #' Set Cache Root Path
 #'
-#' @param path the cache root directory
+#' @param path the cache root directory (DEFAULT: a temporary directory, 
+#'   but this can be change to a more permanent location) 
 #' 
 #' @examples 
 #' setCacheRootPath()
-#'
+#' \dontrun{
+#' setCacheRootPath("~/.simpleRCache")
+#' }
+#' 
 #' @export
-setCacheRootPath <- function(path="~/.simpleRCache") {
+setCacheRootPath <- function(path=tempdir()) {
   if(!file.exists(path)) {
     dir.create(path)
   }
